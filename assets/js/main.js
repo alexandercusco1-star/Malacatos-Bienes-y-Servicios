87392b8fe203fc576e6255d301f0ac37467665da
alexandercusco1-star <alexandercusco1@gmail.com>
2025-12-05 23:52:57 -0500

    Delete assets/js/main.js

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// main.js – Inicialización
-
-document.addEventListener("DOMContentLoaded", () => {
-    if (typeof iniciarApp === "function") {
-        iniciarApp();
-    } else {
-        console.error("No se encontró la función iniciarApp");
-    }
-});
