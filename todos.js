aecc31eb83898c4ec0afa36257df697ba1297da7
alexandercusco1-star <alexandercusco1@gmail.com>
2025-12-09 21:47:43 -0500

    Update todos.js

--- todos.js
+++ todos.js
@@ -1,1 +1,24 @@
+async function cargar(r){ const x = await fetch(r); return await x.json(); }
 
+async function iniciarTodos(){
+    const bienes = await cargar("data/bienes.json");
+    const servicios = await cargar("data/servicios.json");
+
+    render("todos-bienes", bienes);
+    render("todos-servicios", servicios);
+}
+
+function render(id, arr){
+    const box = document.getElementById(id);
+    box.innerHTML = arr.map(it => {
+        const img = it.imagenes?.[0] ? `data/${it.imagenes[0]}` : '';
+        return `
+        <div class="tarjeta">
+            ${img ? `<img src="${img}">` : ''}
+            <h3>${it.nombre}</h3>
+            <p style="color:var(--muted); font-size:14px">${it.descripcion || it.direccion || ''}</p>
+        </div>`;
+    }).join("");
+}
+
+iniciarTodos();
