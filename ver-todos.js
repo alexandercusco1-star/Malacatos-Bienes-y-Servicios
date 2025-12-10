900bf000b1603bc4b04c5cc7aca618eae513693a
alexandercusco1-star <alexandercusco1@gmail.com>
2025-12-09 23:05:00 -0500

    Update ver-todos.js

--- ver-todos.js
+++ ver-todos.js
@@ -1,1 +1,19 @@
+async function loadAll(){
+  const bienes = await (await fetch("data/bienes.json")).json();
+  const servicios = await (await fetch("data/servicios.json")).json();
 
+  const cont = document.getElementById("todos");
+  cont.innerHTML = "";
+
+  [...bienes, ...servicios].forEach(item=>{
+    cont.innerHTML += `
+      <div class="card">
+        <img src="data/${item.imagenes[0]}">
+        <h3>${item.nombre}</h3>
+        <p>${item.descripcion || item.direccion}</p>
+      </div>
+    `;
+  });
+}
+
+loadAll();
