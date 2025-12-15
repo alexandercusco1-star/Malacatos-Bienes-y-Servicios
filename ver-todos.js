f121cafb745f14ca3879ea2ed25f32298d40e08f
alexandercusco1-star <alexandercusco1@gmail.com>
2025-12-14 19:22:47 -0500

    Update ver-todos.js

--- ver-todos.js
+++ ver-todos.js
@@ -3,27 +3,47 @@ async function cargar(ruta) {
   return r.json();
 }
 
+let currentGallery = [];
+let galleryIndex = 0;
+
+function abrirGaleria(imagenes) {
+  currentGallery = imagenes;
+  galleryIndex = 0;
+  document.getElementById("lb-img").src = "data/" + currentGallery[0];
+  document.getElementById("lightbox").classList.add("open");
+}
+
+function cambiarImg(dir) {
+  galleryIndex = (galleryIndex + dir + currentGallery.length) % currentGallery.length;
+  document.getElementById("lb-img").src = "data/" + currentGallery[galleryIndex];
+}
+
+document.getElementById("lb-close").onclick = () =>
+  document.getElementById("lightbox").classList.remove("open");
+
 async function iniciar() {
   const bienes = await cargar("data/bienes.json");
   const servicios = await cargar("data/servicios.json");
 
   const todos = [...bienes, ...servicios];
   const cont = document.getElementById("lista-todos");
-
   cont.innerHTML = "";
 
   todos.forEach(item => {
-    const img = item.imagenes?.[0] ? "data/" + item.imagenes[0] : "";
-    const mas = item.imagenes && item.imagenes.length > 1;
+    const img = item.imagenes?.[0] ? `data/${item.imagenes[0]}` : "";
 
     cont.innerHTML += `
       <div class="tarjeta">
         ${img ? `<img src="${img}">` : ""}
         <h3>${item.nombre}</h3>
         <p>${item.descripcion || ""}</p>
-        ${mas ? `<button onclick='abrirGaleria(${JSON.stringify(item.imagenes)})'>Ver imágenes</button>` : ""}
+        ${item.imagenes && item.imagenes.length > 1
+          ? `<button onclick='abrirGaleria(${JSON.stringify(item.imagenes)})'>Ver más</button>`
+          : ""
+        }
       </div>
     `;
   });
 }
+
 iniciar();
